2ff34bd1bcf717896bc70e6d8f87dd6cf626b649
OcVladislav <vladislav.oculovici@endava.com>
2022-06-24 18:09:57 +0300

    Implement DIP

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,6 +1,6 @@
+import { Page } from "../components/page/Page";
 import { $ } from "../core/dom";
-import { Page } from "../core/Page";
-import { createRecordsTable } from "./dashboard.functions";
+import { createRecordsTable } from "../shared/dashboard.functions";
 
 export class DashboardPage extends Page {
   getRoot() {
